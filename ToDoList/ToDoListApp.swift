da57ed57d74dfd2fc02981d90b4068326c23c2ca
Ivan <mozgovoy2023@gmail.com>
2023-11-09 16:55:15 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Иван Мозговой on 09.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
